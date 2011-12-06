350a133a6075c612d8ccb0644df3775739df6104
János Pásztor <net@janoszen.hu>
2011-12-06 22:28:10 +0100

    Added double include check to bootstrap/unittest.php

--- bootstrap/unittest.php
+++ bootstrap/unittest.php
@@ -1,17 +1,20 @@
 <?php
 
-$classroot = dirname(__DIR__) . \DIRECTORY_SEPARATOR . "src";
-$testroot = dirname(__DIR__) . \DIRECTORY_SEPARATOR . "testclasses";
+if (!defined('TESTBOOTSTRAPPED')) {
+	$classroot = dirname(__DIR__) . \DIRECTORY_SEPARATOR . "src";
+	$testroot = dirname(__DIR__) . \DIRECTORY_SEPARATOR . "testclasses";
 
-require($classroot . \DIRECTORY_SEPARATOR . 'ClassLoader.php');
-\ClassLoader::setClassPath(
-	array(
-		$classroot,
-		$testroot,
-	)
-);
-unset($classroot);
-unset($testroot);
+	require($classroot . \DIRECTORY_SEPARATOR . 'ClassLoader.php');
+	\ClassLoader::setClassPath(
+		array(
+			$classroot,
+			$testroot,
+		)
+	);
+	unset($classroot);
+	unset($testroot);
 
-define('TESTDATADIR', dirname(__DIR__) . \DIRECTORY_SEPARATOR . 'testdata');
-define('TESTTMPDIR', dirname(__DIR__) . \DIRECTORY_SEPARATOR . 'testdata' . \DIRECTORY_SEPARATOR . 'tmp');
+	define('TESTDATADIR', dirname(__DIR__) . \DIRECTORY_SEPARATOR . 'testdata');
+	define('TESTTMPDIR', dirname(__DIR__) . \DIRECTORY_SEPARATOR . 'testdata' . \DIRECTORY_SEPARATOR . 'tmp');
+	define('TESTBOOTSTRAPPED', true);
+}
